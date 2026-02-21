62842323ec34e409cf2392db07485ea6e88f9fe1
Rishindra <rishdavulu@utexas.edu>
2026-02-21 01:03:41 +0100

    re-running the github actions after

--- backend-python/check_models.py
+++ backend-python/check_models.py
@@ -1,16 +1,16 @@
-from google import genai
-import os
+# from google import genai
+# import os
 
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-client = genai.Client(api_key=GEMINI_API_KEY)
+# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+# client = genai.Client(api_key=GEMINI_API_KEY)
 
-print("Checking available models...")
-try:
-    # List all models available to your API key
-    for m in client.models.list():
-        if "generateContent" in m.supported_actions:
-            print(f"- {m.name}")
+# print("Checking available models...")
+# try:
+#     # List all models available to your API key
+#     for m in client.models.list():
+#         if "generateContent" in m.supported_actions:
+#             print(f"- {m.name}")
             
-except Exception as e:
-    print(f"Error: {e}")
+# except Exception as e:
+#     print(f"Error: {e}")
